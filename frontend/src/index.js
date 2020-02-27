f74ea53efd3531eda1480d005ab0e0bb11744174
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-02-27 20:15:53 +0600

    created Structure(frontend)

---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import thunkMiddleware from 'redux-thunk';
+import {applyMiddleware, createStore, compose} from "redux";
+import {BrowserRouter} from 'react-router-dom';
+import reducer from "./store/reducer";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+serviceWorker.unregister();
---
+++ frontend/src/store/actions.js
@@ -0,0 +1,4 @@
+export const POST_LINK_SUCCESS = 'POST_LINK_SUCCESS';
+
+export const createLinkSuccess = link => ({type: POST_LINK_SUCCESS, link});
+
